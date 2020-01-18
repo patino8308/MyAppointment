03bc2eb860fa868026d67fe6f2e7921408e49764
edwin samir <patino8308@gmail.com>
2020-01-18 18:12:02 +0100

    cambio para mostrar la horas

--- app/Services/ScheduleService.php
+++ app/Services/ScheduleService.php
@@ -29,8 +29,13 @@ class ScheduleService implements ScheduleServiceInterface
             ->where('user_id', $doctorId)
             ->first(['morning_start', 'morning_end', 'afternoon_start', 'afternoon_end']);
 
-        if (!$workDay) {
-            return [];
+        if ($workDay) {
+            $morningIntervals = $this->getIntervals($workDay->morning_start, $workDay->morning_end, $date, $doctorId);
+
+            $afternoonIntervals = $this->getIntervals($workDay->afternoon_start, $workDay->afternoon_end, $date, $doctorId);
+        } else {
+            $morningIntervals = [];
+            $afternoonIntervals = [];
         }
 
         $morningIntervals = $this->getIntervals($workDay->morning_start, $workDay->morning_end, $date, $doctorId);
