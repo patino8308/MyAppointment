148c9c8415d0f75e7060c8bd63f91cdb9465074d
edwin samir <patino8308@gmail.com>
2020-01-27 22:10:38 +0100

    creacion de token para fcm

--- routes/api.php
+++ routes/api.php
@@ -32,5 +32,5 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/appointments', 'AppointmentsController@index');
 
     //FCM
-    Route::get('/fcm/tokem', 'FirebaseController@postToken');
+    Route::get('/fcm/token', 'FirebaseController@postToken');
 });
